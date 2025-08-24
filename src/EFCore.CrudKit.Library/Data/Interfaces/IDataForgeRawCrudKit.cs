65fa4bf5ea1ec80a5c95314b41b1364fbc130950
Toba Ojo <ojotobar@gmail.com>
2025-08-24 19:09:29 +0100

    Implemented dapper and fluent query builders

---
+++ src/EFCore.CrudKit.Library/Data/Interfaces/IDataForgeRawCrudKit.cs
@@ -0,0 +1,10 @@
+﻿
+namespace EFCore.CrudKit.Library.Data.Interfaces
+{
+    public interface IDataForgeRawCrudKit
+    {
+        Task<bool> ExecuteQueryAsync(string query, object? param = null);
+        Task<List<T>> FindAsync<T>(string query, object? param = null) where T : class;
+        Task<T?> FindOneAsync<T>(string query, object? param = null) where T : class;
+    }
+}
---
+++ src/EFCore.CrudKit.Library/Extensions/StringExtension.cs
@@ -0,0 +1,35 @@
+﻿namespace EFCore.CrudKit.Library.Extensions
+{
+    public static class StringExtension
+    {
+        public static string ToQuotedCsv(this string[] values)
+        {
+            return string.Join(", ", values.Select(v => $"'{v}'"));
+        }
+
+        public static string ToCsv(this string[] values)
+        {
+            return string.Join(", ", values.Select(v => $"{v}"));
+        }
+
+        public static string ToQuotedCsv(this List<Guid> values)
+        {
+            return string.Join(", ", values.Select(v => $"'{v}'"));
+        }
+
+        public static string ToCsv(this List<Guid> values)
+        {
+            return string.Join(", ", values.Select(v => $"{v}"));
+        }
+
+        public static string ToCsv(this List<object> values)
+        {
+            return string.Join(", ", values.Select(v => $"{v}"));
+        }
+
+        public static string ToQuotedCsv<T>(this List<T> values)
+        {
+            return string.Join(" ,", values.Select(v => $"'{v}'"));
+        }
+    }
+}
