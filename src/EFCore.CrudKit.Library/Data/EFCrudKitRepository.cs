cc280a7dd86d05f926788f62e07c380ba4666ac1
Toba Ojo <ojotobar@gmail.com>
2025-10-12 13:52:05 +0100

    Added repository class for direct interactions with the database

---
+++ src/EFCore.CrudKit.Library/Data/EFCrudKitRepository.cs
@@ -0,0 +1,75 @@
+﻿using EFCore.CrudKit.Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace EFCore.CrudKit.Library.Data
+{
+    public class EFCrudKitRepository<TEntity, UContext> 
+        where TEntity : EntityBase 
+        where UContext : DbContext
+    {
+        private readonly UContext _context;
+
+        public EFCrudKitRepository(UContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TEntity?> FindByIdAsync(Guid id,
+                                                 bool track = false,
+                                                 CancellationToken cancellation = default)
+        {
+            return track ?
+                await _context.Set<TEntity>()
+                    .FindAsync(id, cancellation) : 
+                await _context.Set<TEntity>().AsNoTracking()
+                    .FirstAsync(e => e.Id == id, cancellation);
+        }
+
+        public async Task AddAsync(TEntity entity,
+                                   bool save = true,
+                                   CancellationToken cancellation = default)
+        {
+            await _context.Set<TEntity>().AddAsync(entity, cancellation);
+            if (save)
+            {
+                await SaveAsync(cancellation);
+            }
+        }
+
+        public async Task UpdateAsync(TEntity entity,
+                                      bool save = true,
+                                      CancellationToken cancellation = default)
+        {
+            _context.Set<TEntity>().Update(entity);
+            if (save)
+            {
+                await SaveAsync(cancellation);
+            }
+        }
+
+        public async Task DeleteAsync(TEntity entity,
+                                      bool save = true,
+                                      CancellationToken cancellation = default)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            if (save)
+            {
+                await SaveAsync(cancellation);
+            }
+        }
+
+        public IQueryable<TEntity> Query(Expression<Func<TEntity, bool>> expression)
+        {
+             return _context.Set<TEntity>()
+                .Where(expression)
+                .AsNoTracking()
+                .AsQueryable();
+        }
+
+        public async Task<bool> SaveAsync(CancellationToken cancellation)
+        {
+            return await _context.SaveChangesAsync(cancellation) > 0;
+        }
+    }
+}
